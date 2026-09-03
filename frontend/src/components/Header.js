b2941636dd9433071636a758f05588480d9397ac
Leevy-Otieno <leevyotieno@gmail.com>
2026-09-03 16:37:31 -0700

    header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,43 @@
 import React from 'react'
 import Logo from './Logo'
+import { IoSearch } from "react-icons/io5";
+import { FaRegUserCircle } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+
+
+
 
 const Header = () => {
   return (
   <header className="h-17 shadow-sm">
-    <div className="h-full container mx-auto flex items-center px-4">
+    <div className="h-full container mx-auto flex items-center px-4 justify-between">
       <div className="">
         <Logo w={100} h={50} />
+      </div>
+      <div>
+        <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounderd-full focus-within:shadow-pl-2">
+          
+          <input type="text" placeholder="Search product here..." className="w-full outline-none  " />
+          <div className="text-lg min-w-[50px] h-8 bg-amber-500 flex items-center justify-center rounded-r-full text-white">
+            <IoSearch/>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center gap-7
+      ">
+      <div className="text-3xl cursor-pointer">
+       <FaRegUserCircle />
+      </div>
+      <div className="text-2xl relative cursor-pointer">
+       <span> <FaShoppingCart /></span>
+
+       <div className="bg-amber-500 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-0 -right-3 ">
+        <p className="text-xs">0</p>
+       </div>
+      </div>
+      <div>
+        <button className="px-2 py-1 bg-amber-400 rounded-full text-white hover:bg-amber-600">Login</button>
+      </div>
       </div>
     </div>
   </header>

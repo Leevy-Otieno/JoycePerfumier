08ebc93878efcbd7e6dd77a92af06984978dbc46
Leevy-Otieno <leevyotieno@gmail.com>
2026-09-10 16:03:21 -0700

    7thcommit

--- backend/controller/userSignUp.js
+++ backend/controller/userSignUp.js
@@ -1,12 +1,11 @@
-async function userSignUpController(req,res){
-    try{
-
-    }
-    catch(err){
-res.json({
-    message : err ,
-    error : true,
-    success : false
-});
-    }
+async function userSignUpController(req, res) {
+  try {
+    const { email, password, name };
+  } catch (err) {
+    res.json({
+      message: err,
+      error: true,
+      success: false,
+    });
+  }
 }

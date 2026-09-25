92797d9fe96afea8ac1817cc4f5a9887e4ae5f5a
Leevy-Otieno <leevyotieno@gmail.com>
2026-09-24 19:33:55 -0700

    vercel err

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='bg-slate-200'>
       <div className='container mx-auto p-4'>
-       <p className='text-center font-bold' title="Youtube Channel">Leevy Otieno</p>
+       <p className='text-center font-bold' title="Youtube Channel">Powered by LeevyStack.</p>
       </div>
     </footer>
   )
